770b27d593e80e52db15be6b7ffdbc0bfb7d082e
Fujiken0705 <kentofujita.new@gmail.com>
2023-03-09 10:57:45 +0900

    Initial Commit

---
+++ Kinnee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kinnee
+//
+//  Created by KentoFujita on 2023/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

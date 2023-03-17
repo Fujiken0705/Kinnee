4cb8afa0e1c66b962dd8e8e212e4c623b75c8078
Fujiken0705 <kentofujita.new@gmail.com>
2023-03-17 16:59:52 +0900

    UIの配置、紐つけ 20230317

--- Kinnee/DetailViewController.swift
+++ Kinnee/DetailViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class DetailViewController: UIViewController{
-    
 
+    @IBOutlet weak var detailNameLabel: UILabel!
+    @IBOutlet weak var detailRepLabel: UILabel!
+    @IBOutlet weak var detailSetLabel: UILabel!
+    @IBOutlet weak var detailSearchButton: UIButton!
+    @IBOutlet weak var setContollellLabel: UILabel!
+    @IBOutlet weak var setCompleteButton: UIButton!
+
+    
 }
